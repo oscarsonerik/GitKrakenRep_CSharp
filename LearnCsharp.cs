9f1be0f9222126374b5bd6479f07ecdb05d2a9ef
Erik Oscarson <erik.oscarson97@gmail.com>
2024-12-09 00:59:34 +0100

    La till write

--- LearnCsharp.cs
+++ LearnCsharp.cs
@@ -1,1 +1,14 @@
-#Test
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skriver ut text i konsolen
+        Console.WriteLine("Hej, världen!");
+
+        // Väntar på att användaren trycker på en tangent innan programmet avslutas
+        Console.WriteLine("Tryck på en valfri tangent för att avsluta...");
+        Console.ReadKey();
+    }
+}
